333ab065207580756ddf9782ea71060bbed44e01
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-28 20:50:00 -0500

    Class 8 - Static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,14 @@
 public class Doctor {
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Constructing Doctor");
+        id++;
     }
     Doctor(String name){
+        id++;
         System.out.println("The Doctor's name is: " + name);
         //this.name = name;
     }
@@ -14,5 +16,9 @@ public class Doctor {
     //Behaviors
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
