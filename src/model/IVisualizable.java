3b53fbe86213ef9048ff3a668ba43a717d01246f
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-29 15:19:36 -0500

    Class 31 - inheritance in interfaces Part 2

--- src/model/IVisualizable.java
+++ src/model/IVisualizable.java
@@ -6,4 +6,9 @@ public interface IVisualizable extends IReadable, Serializable {
     public void setViewed();
     public Boolean isViewed();
     public String timeViewed();
+
+    @Override
+    default void read() {
+        // TODO Auto-generated method stub
+    }
 }
