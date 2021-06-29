8a3e94506a5f369b37a53553a1be0d31d7ce0c3f
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-29 10:30:35 -0500

    Class 12 - Variable vs Object Memory

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,36 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        showMenu();
+        //showMenu();
         Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatric");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        //System.out.println(myDoctor.name);
+        //System.out.println(myDoctor.speciality);
+
+        int i = 0;
+        int b = 2;
+        b = i;
+        String name = "Ann";
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahí", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        /*patient.setWeight(54.6);
+        System.out.println(patient.getName());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());*/
     }
 }
