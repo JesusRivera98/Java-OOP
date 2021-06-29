68e55ed23cbc6d3a0af799877af162d822503b71
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-06-29 11:23:17 -0500

    Class 16 - nested classes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
         //showMenu();
         Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatric");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        int i = 0;
-        int b = 2;
-        b = i;
-        String name = "Ann";
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
 
         System.out.println();
         System.out.println();
